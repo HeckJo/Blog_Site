ccc969e627da72a4a36bc4df6fc53c69a78609ca
j_heck10 <j_heck10@uni-muenster.de>
2022-09-15 20:10:23 +0200

    added Header for Solid-Bowel Page. Functionality missing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
